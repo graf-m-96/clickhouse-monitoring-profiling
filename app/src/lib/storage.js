725b6d9106bad3e7f8db82304151a428e3382664
Мамонов Антон <graf.m.96@yandex.ru>
2020-02-02 21:54:13 +0500

    Обработал ошибки от localstorage

--- app/src/lib/storage.js
+++ app/src/lib/storage.js
@@ -3,29 +3,47 @@ const connectionsKey = `${commonKey}/connection`;
 const queriesKey = `${commonKey}/queries`;
 
 export const getConnections = () => {
-    const value = localStorage.getItem(connectionsKey);
+    try {
+        const value = localStorage.getItem(connectionsKey);
 
-    if (value === null) {
-        return [];
+        if (value === null) {
+            return [];
+        }
+    
+        return JSON.parse(value);
+    } catch (error) {
+        console.error(error);
     }
-
-    return JSON.parse(value);
 };
 
 export const saveConnections = connections => {
-    localStorage.setItem(connectionsKey, JSON.stringify(connections));
+    try {
+        localStorage.setItem(connectionsKey, JSON.stringify(connections));
+    } catch (error) {
+        console.error(error);
+    }
 };
 
 export const getQueries = () => {
-    const value = localStorage.getItem(queriesKey);
+    try {
+        const value = localStorage.getItem(queriesKey);
 
-    if (value === null) {
+        if (value === null) {
+            return [];
+        }
+    
+        return JSON.parse(value);
+    }
+    catch (error) {
+        console.error(error);
         return [];
     }
-
-    return JSON.parse(value);
 };
 
 export const saveQueries = queries => {
-    localStorage.setItem(queriesKey, JSON.stringify(queries));
+    try {
+        localStorage.setItem(queriesKey, JSON.stringify(queries));
+    } catch (error) {
+        console.error(error);
+    }
 };
